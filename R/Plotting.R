87b70ce32469c52300b9b89247e23a0072afb2e6
Martinez Rea David <david.martinezrea@health.fgov.be>
2023-05-18 14:09:45 +0200

    plotting sigma with factor

--- R/Plotting.R
+++ R/Plotting.R
@@ -30,17 +30,17 @@ graph = function(fit){
   }
   sigma = sigma*sqrt(laguerre_var(fit$theta, fit$theta_tilde,fit$quantile))
   s = as.matrix(seq(from=min(X[,2]), to=max(X[,2]), by=0.01))
-  s_smooth = (exp(Her(s, deg = deg, type=type)%*%H)/exp(1))*sqrt(laguerre_var(fit$theta, fit$theta_tilde,fit$quantile))
+  #s_smooth = (exp(Her(s, deg = deg, type=type)%*%H)/exp(1))*sqrt(laguerre_var(fit$theta, fit$theta_tilde,fit$quantile))
   data = as.data.frame(cbind(X[,2],X_s, sigma,X%*%beta, Y,Delta))
-  colnames(data) = c("x", "x_s", "sigma", "quantile", "Y", "Delta")
-  data2 = as.data.frame(cbind(s,s_smooth))
-  colnames(data2) = c("s", "s_smooth")
-  if(dim(X_s)[2]==1){
-    print(ggplot2::ggplot(data, ggplot2::aes_string(x = "x", y = "sigma")) + ggplot2::geom_point() + ggplot2::geom_line(color="orange") +
+  colnames(data) = c("x", "x_s", "factor", "sigma", "quantile", "Y", "Delta")
+  data2 = as.data.frame(cbind(s))
+  colnames(data2) = c("s")
+  if(dim(X_s)[2] %in% c(1, 2)){
+    print(ggplot2::ggplot(data, ggplot2::aes_string(x = "x", y = "sigma", group = "factor")) + ggplot2::geom_point() + ggplot2::geom_line(color="orange") +
             ggplot2::ggtitle("sigma(x)"))
 
-    print(ggplot2::ggplot(data2, ggplot2::aes_string(x = "s", y = "s_smooth")) + ggplot2::geom_point() + ggplot2::geom_line(color="green") +
-            ggplot2::ggtitle("sigma(x) smooth"))
+    #print(ggplot2::ggplot(data2, ggplot2::aes_string(x = "s", y = "s_smooth")) + ggplot2::geom_point() + ggplot2::geom_line(color="green") +
+     #       ggplot2::ggtitle("sigma(x) smooth"))
   }
   print(ggplot2::ggplot(data, ggplot2::aes_string(x = "x", y = "Y", colour = as.factor(Delta))) + ggplot2::geom_point() +
           ggplot2::geom_line(data=data, ggplot2::aes_string(x = "x", y = "quantile"), color="black") +
